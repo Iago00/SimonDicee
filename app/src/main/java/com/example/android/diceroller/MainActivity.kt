2a03de093286f552990d31b48b7572f6b8269e8d
iago00 <Iago-2001>
2020-11-23 09:34:34 +0100

    listo

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -15,42 +15,96 @@
  */
 
 package com.example.android.diceroller
-
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    // contenedor de imagen
-    // modificando el main
-    lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // cargamos layout
         setContentView(R.layout.activity_main)
+        var sec: MutableList<Int> = mutableListOf<Int>()
+        var user_sec: MutableList<Int> = mutableListOf<Int>()
+        var finished  = false
+        val button_red : Button = findViewById(R.id.rojo)
+        val button_yellow : Button = findViewById(R.id.amarillo)
+        val button_blue : Button = findViewById(R.id.azul)
+        val button_green : Button = findViewById(R.id.verde)
+        val button_start : Button = findViewById(R.id.start)
+        val button_check : Button = findViewById(R.id.check)
+        val toast = Toast.makeText(applicationContext,"Juego terminado", Toast.LENGTH_SHORT)
+        val toast2 = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
 
-        val botonTirar: Button = findViewById(R.id.roll_button)
-        botonTirar.setOnClickListener {
-            rollDice()
+        button_start.setOnClickListener{
+            finished = false
+            reset(sec,user_sec)
+            addToSecu(sec)
+            toast2.show()
+            showSec(sec)
         }
 
-        diceImage = findViewById(R.id.dice_image)
+        button_check.setOnClickListener{
+            if(finished==false){
+                if(checkSec(sec,user_sec)){
+                    addToSecu(sec)
+                    user_sec.clear()
+                    showSec(sec)
+                }else{
+                    finished=true
+                    toast.show()
+                }
+            }
+        }
+        button_red.setOnClickListener{
+            addUserSec(user_sec,1)
+        }
+        button_green.setOnClickListener{
+            addUserSec(user_sec,2)
+        }
+        button_yellow.setOnClickListener{
+            addUserSec(user_sec,3)
+        }
+        button_blue.setOnClickListener{
+            addUserSec(user_sec,4)
+        }
+        //showSec(sec)
+
     }
 
-    private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
+    fun addToSecu(sec : MutableList<Int>)  {
+        val numb= Random.nextInt(4) + 1
+        sec.add(numb)
+    }
 
-        diceImage.setImageResource(drawableResource)
+    fun checkSec(sec : MutableList<Int>, secUsr : MutableList<Int>) : Boolean {
+        return sec == secUsr
+    }
+
+    fun reset(sec: MutableList<Int>, secUsr: MutableList<Int>){
+        sec.clear()
+        secUsr.clear()
+    }
+
+    fun addUserSec(secUsr: MutableList<Int>, color: Int){
+        when(color){
+            1 -> secUsr.add(1)
+            2 -> secUsr.add(2)
+            3 -> secUsr.add(3)
+            else -> secUsr.add(4)
+        }
+    }
+
+    fun showSec(sec: MutableList<Int>){
+        var t = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
+        for (color in sec){
+            when(color){
+                1 -> Toast.makeText(applicationContext,"Rojo", Toast.LENGTH_SHORT).show()
+                2 -> Toast.makeText(applicationContext,"Verde", Toast.LENGTH_SHORT).show()
+                3 -> Toast.makeText(applicationContext,"Amarillo", Toast.LENGTH_SHORT).show()
+                4 -> Toast.makeText(applicationContext,"Azul", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
